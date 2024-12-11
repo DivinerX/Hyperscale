23dd1ecb0e6a75fd504ad1f31e1f67f1452e3fd5
DivinerX <loc_yan@outlook.com>
2024-12-10 21:45:00 -0300

    restructure adding server

--- web/src/components/Chatboard/MessageInput/MessageInput.tsx
+++ web/src/components/Chatboard/MessageInput/MessageInput.tsx
@@ -10,7 +10,7 @@ export const MessageInput: FC<MessageInputProps> = ({ status, message, setMessag
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#0A0A0A] border-gray-800 border-t px-4 py-2">
       <span className="text-sm text-gray-500 block mb-2">{status}</span>
-      <div className="flex items-center bg-black">
+      <div className="flex items-center bg-black border-[0.25px] border-gray-800">
         <input
           type="text"
           placeholder="CLICK HERE TO TYPE"
--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
 import { FC } from "react";
 import logo from "../../assets/logo.svg";
 import verified from "../../assets/success.svg";
-import avatar from "/avatar.png"
+import avatar from "/avatar.png";
 
-export const Header: FC = () => {
+interface HeaderProps {
+  onlineUsers: number;
+  totalUsers: number;
+  builtDate: string;
+  version: string;
+  user: {
+    name: string;
+    isVerified: boolean;
+  };
+}
+
+export const Header: FC<HeaderProps> = ({ onlineUsers, totalUsers, builtDate, version, user }) => {
   return (
     <header className="bg-black text-white px-2 py-2 flex justify-between items-center text-xs border-b-[0.25px] border-[#858585]">
       <div className="flex gap-2">
@@ -17,23 +28,23 @@ export const Header: FC = () => {
         </div>
         <div className="flex flex-col justify-center px-3">
           <span className="text-[#8D8D8D]">USER: ONLINE</span>
-          <span className="flex items-center gap-1"><div className="w-[5px] h-[5px] rounded-full bg-green-500"></div> {24}/{64}</span>
+          <span className="flex items-center gap-1"><div className="w-[5px] h-[5px] rounded-full bg-green-500"></div> {onlineUsers}/{totalUsers}</span>
         </div>
         <div className="h-10 w-[1px] bg-white/30 mx-2"></div>
         <div className="flex flex-col justify-center">
           <span className="text-[#8D8D8D]">BUILT</span>
-          <span className="text-[#8D8D8D]">{`2023-11-02 20:35`}</span>
+          <span className="text-[#8D8D8D]">{builtDate}</span>
         </div>
         <div className="flex flex-col justify-center px-3">
           <span className="text-[#8D8D8D]">BUILD MODEL</span>
-          <span>{`v.0.0.1`}</span>
+          <span>{version}</span>
         </div>
       </div>
       {/* right side of the header */}
       <div className="flex items-center gap-1">
         <div className="flex flex-col justify-center items-end px-3">
           <span className="text-[#8D8D8D] uppercase">logged in as</span>
-          <span className="flex items-center gap-1">{`@solamimaxi`} <img src={verified} alt="verified" className="w-4 h-4" /></span>
+          <span className="flex items-center gap-1">{`${user.name}`} <img src={verified} alt="verified" className="w-4 h-4" /></span>
         </div>
         <div>
           <img src={avatar} alt="avatar" className="w-10 h-10 rounded-sm" />
