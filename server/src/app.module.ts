b59ee5710d4e73f9f5e95c31bac5063e26fd6655
DivinerX <loc_yan@outlook.com>
2024-12-10 22:43:05 -0300

    mongo connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoConfig from './config/mongo.config';
+import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [mongoConfig],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [DatabaseService],
 })
 export class AppModule {}
