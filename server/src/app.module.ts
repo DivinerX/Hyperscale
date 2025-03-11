1b9009761322d8552ef281e415bd5cb33686aa06
DivinerX <loc_yan@outlook.com>
2025-03-11 14:19:01 +0900

    auth.module added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { databaseConfig, jwtConfig } from './config/env.config';
 import { DatabaseService } from './database.service';
 import { SocketGateway } from './gateways/socket.gateway';
-import { AuthModule } from './auth/auth.module';
 import { MessageModule } from './message/message.module';
 import { MessageService } from './message/message.service';
 import { Message, MessageSchema } from './message/message.schema';
@@ -27,7 +26,6 @@ import { Coin } from './coin/coin.schema';
     }),
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
     MongooseModule.forFeature([{ name: Coin.name, schema: CoinSchema }]),
-    AuthModule,
     MessageModule,
     CoinModule,
   ],
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,29 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TwitterStrategy } from './twitter.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
-import { ConfigService } from '@nestjs/config';
-import { ConfigModule } from '@nestjs/config';
-import { TwitterStrategy } from './twitter.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'twitter' }),
+    ConfigModule.forRoot(),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt_secret.secret'),
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '60m' },
       }),
       inject: [ConfigService],
     }),
   ],
-  providers: [
-    AuthService,
-    TwitterStrategy,
-  ],
   controllers: [AuthController],
+  providers: [AuthService, TwitterStrategy],
 })
 export class AuthModule {}
